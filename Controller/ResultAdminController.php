2a8b53fd21764a07fec87728e8ce6a2faa80c211
Matija Martinic <crazy4magic@gmail.com>
2014-07-21 10:38:04 +0200

    Working on service for results

---
+++ Controller/ResultAdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Netgen\LiveVotingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ResultAdminController extends Controller{
+
+    public function ViewResultsAction(Request $request, $event_id){
+        $results = $this->get('live_voting.result')->getResults($event_id);
+        print_r($results);
+        return $this->render('LiveVotingBundle:Result:index.html.twig');
+    }
+
+}
+
+?>
---
+++ Features/Result/Result.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Netgen\LiveVotingBundle\Features\Result;
+
+use Doctrine\ORM\
+
+class Result {
+    public function getResults($event_id){
+
+    }
+} 
