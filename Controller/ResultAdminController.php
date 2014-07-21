1be51a4d5aaaf7789cecbe78ce3b0586d51eff58
Matija Martinic <crazy4magic@gmail.com>
2014-07-21 16:21:37 +0200

    Added results

--- Controller/ResultAdminController.php
+++ Controller/ResultAdminController.php
@@ -10,7 +10,8 @@ class ResultAdminController extends Controller{
     public function ViewResultsAction(Request $request, $event_id){
         $results = $this->get('live_voting.result')->getResults($event_id);
 
-        return $this->render('LiveVotingBundle:Result:index.html.twig', array('users'=>$results));
+        return $this->render('LiveVotingBundle:Result:index.html.twig', array('animation_data'=>
+            json_encode($results)));
     }
 
 }
--- Features/Result/Result.php
+++ Features/Result/Result.php
@@ -16,26 +16,52 @@ class Result {
     public function getResults($event_id){
         $event = $this->em->getRepository('LiveVotingBundle:Event')->find($event_id);
         if( !$event instanceof Event){
-            // TODO: Ask Edi how what to return to controller and how to handle exceptions
+            // TODO: Ask Edi what to return to controller and how to handle exceptions
         }
 
-        $result = array();
-
         $votes = $this->em->getRepository('LiveVotingBundle:Vote')->findByEvent($event);
-        $groupedUsers = array();
+        $presentations = $this->em->getRepository('LiveVotingBundle:Presentation')->findByEvent($event);
+        $groupedVotes = array(); // Grouped votes by user
         foreach($votes as $vote){
 
-            if( isset($groupedUsers[$vote->getUser()->getId()] ){
-
+            if( isset($groupedVotes[$vote->getUser()->getId()]) ){
+                array_push($groupedVotes[$vote->getUser()->getId()], $vote);
             }else{
-                $groupedUsers[$vote->getUser()->getId()] = array($vote);
+                $groupedVotes[$vote->getUser()->getId()] = array($vote);
             }
 
         }
 
-        foreach($groupedUsers as $user){
+        $result = array(); // array of animation keyframes
+        $presentationResult = array();
+        foreach($presentations  as $presentation){
+            $presentationResult[$presentation->getId()] = array(
+                'numOfUsers'=>0,
+                'score'=>0,
+                'average'=>0,
+                'presentation'=>$presentation->getPresentationName(),
+                'presenter'=>array(
+                    'name'=>$presentation->getPresenterName(),
+                    'surname'=>$presentation->getPresenterSurname()
+                )
+
+            );
+        }
+        foreach($groupedVotes as $userId=>$votes){
+            // $result[$userId] = $presentationResult;
+            $tmp1 = $presentationResult;
+            foreach($votes as $vote){
+                $tmp = $presentationResult[$vote->getPresentation()->getId()];
+                $tmp['numOfUsers']++;
+                $tmp['score'] += $vote->getRate();
+                $tmp['average'] = $tmp['score']/$tmp['numOfUsers'];
+                $tmp1[$vote->getPresentation()->getId()]  = $tmp;
+                $presentationResult[$vote->getPresentation()->getId()] = $tmp;
+            }
+            $result[] = $tmp1;
 
         }
-        return $groupedUsers;
+
+        return $result;
     }
 } 
