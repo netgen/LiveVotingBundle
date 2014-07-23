984bfd26f817b149197aa6112f7594e0d6a0b0ff
Matija Martinic <crazy4magic@gmail.com>
2014-07-23 11:44:48 +0200

    Added vote to service

--- Controller/VoteController.php
+++ Controller/VoteController.php
@@ -6,7 +6,7 @@ namespace Netgen\LiveVotingBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-
+use Netgen\LiveVotingBundle\Exception\JsonException;
 use Netgen\LiveVotingBundle\Entity\Vote;
 
 class VoteController extends Controller{
@@ -14,79 +14,12 @@ class VoteController extends Controller{
     // TODO: This is too big. Need to make it smaller or make it like a service.
     public function voteAction(Request $request, $presentation_id){
 
-        $session = $request->getSession();
-        // $session->start();
-        $rate = $request->request->get('rate');
-
-        if(!is_numeric($rate)){
-            return new JsonResponse(array(
-                'error'=>4,
-                'errorMessage'=>'Vote must be a number:'.$rate
-            ));
+        try{
+            $result = $this->get('live_voting.handlerequest')->validateVote($presentation_id);
+            return new JsonResponse($result);
+        }catch(JsonException $e){
+            return new JsonResponse(unserialize($e->getMessage()));
         }
-        $rate = intval($rate);
-
-        $presentation = $this->getDoctrine()->getRepository('LiveVotingBundle:Presentation')->find($presentation_id);
-        if( $presentation==null ){
-            return new JsonResponse(array(
-                'error'=>3,
-                'errorMessage'=>'Unknown presentation.'
-            ));
-        }
-        if( $presentation->getEvent()==null ){
-            return new JsonResponse(array(
-                'error'=>5
-            ));
-        }elseif( $presentation->getEvent()->getStateName()=='PRE' ){ // voting is closed
-            return new JsonResponse(array(
-                'error'=>1,
-                'errorMessage'=>'Voting is disabled.'
-            ));
-        }elseif( $presentation->getEvent()->getStateName()=='POST' ){ // voting ended
-            $date_when_voting_ends = intval($presentation->getEvent()->getStateValue());
-            if ( time()>$date_when_voting_ends ){
-                return new JsonResponse(array(
-                    'error'=>1,
-                    'errorMessage'=>'Voting is closed.'
-                ));
-            }
-        }
-
-        if( $rate<=0 or $rate>5){
-            return new JsonResponse(array(
-                'error'=>3,
-                'errorMessage'=>'Vote must be between 1 and 5.'
-            ));
-        }
-
-        $user = $this->getDoctrine()->getRepository('LiveVotingBundle:User')->find($session->getId());
-        if( $user==null ){
-            return new JsonResponse(array(
-                'error'=>2,
-                'errorMessage'=>'Unknown user.'
-            ));
-        }
-
-
-        $vote = $this->getDoctrine()->getRepository('LiveVotingBundle:Vote')->findOneBy(array(
-            'user'=>$user,
-            'presentation'=>$presentation
-        ));
-
-        if( $vote==null){
-            $vote = new Vote();
-            $vote->setUser($user);
-            $vote->setPresentation($presentation);
-        }
-        $vote->setRate($rate);
-        $vote->setEvent($presentation->getEvent());
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($vote);
-        $em->flush();
-        return new JsonResponse(array(
-            'error'=>0,
-            'errorMessage'=>'Thanks for voting!'
-        ));
 
     }
 
--- Features/Request/ValidateRequest.php
+++ Features/Request/ValidateRequest.php
@@ -76,4 +76,76 @@ class ValidateRequest{
         return $response;
     }
 
+    public function validateVote($presentation_id){
+        $request = $this->requestStack->getCurrentRequest();
+        $session = $request->getSession();
+        $rate = $request->request->get('rate');
+
+        if(!is_numeric($rate)){
+            throw new JsonException(array(
+                'error'=>1,
+                'errorMessage'=>'Vote must be a number.'
+            ));
+        }
+        $rate = intval($rate);
+
+        $presentation = $this->em->getRepository('LiveVotingBundle:Presentation')->find($presentation_id);
+        if( $presentation==null ){
+            throw new JsonException(array(
+                'error'=>1,
+                'errorMessage'=>'Unknown presentation.'
+            ));
+        }
+        if( $presentation->getEvent()->getStateName()=='PRE' ){ // voting is closed
+            throw new JsonException(array(
+                'error'=>1,
+                'errorMessage'=>'Voting is disabled.'
+            ));
+        }elseif( $presentation->getEvent()->getStateName()=='POST' ){ // voting ended
+            $date_when_voting_ends = intval($presentation->getEvent()->getStateValue());
+            if ( time()>$date_when_voting_ends ){
+                throw new JsonException(array(
+                    'error'=>1,
+                    'errorMessage'=>'Voting is closed.'
+                ));
+            }
+        }
+
+        if( $rate<=0 or $rate>5){
+            throw new JsonException(array(
+                'error'=>1,
+                'errorMessage'=>'Vote must be between 1 and 5.'
+            ));
+        }
+
+        $user = $this->em->getRepository('LiveVotingBundle:User')->find($session->getId());
+        if( $user==null ){
+            throw new JsonException(array(
+                'error'=>1,
+                'errorMessage'=>'Unknown user. Please enable cookies.'
+            ));
+        }
+
+
+        $vote = $this->em->getRepository('LiveVotingBundle:Vote')->findOneBy(array(
+            'user'=>$user,
+            'presentation'=>$presentation
+        ));
+
+        if( $vote==null){
+            $vote = new Vote();
+            $vote->setUser($user);
+            $vote->setPresentation($presentation);
+        }
+        $vote->setRate($rate);
+        $vote->setEvent($presentation->getEvent());
+        $em = $this->em;
+        $em->persist($vote);
+        $em->flush();
+        return array(
+            'error'=>0,
+            'errorMessage'=>'Thanks for voting!'
+        );
+    }
+
 }
