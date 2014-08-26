b729cd337e13995a41e514284c0cec8463cc2f27
BornaP <borna.perak@gmail.com>
2014-08-26 16:31:03 +0200

    Modified paths and added page for generating draw pools

--- Controller/PrizeDrawController.php
+++ Controller/PrizeDrawController.php
@@ -9,16 +9,30 @@ use Netgen\LiveVotingBundle\Entity\User;
 use Netgen\LiveVotingBundle\Entity\Vote;
 use Netgen\LiveVotingBundle\Entity\Event;
 
+
 class PrizeDrawController extends Controller {
 
 	public function indexAction(){
 
 		$events = $this->getDoctrine()->getRepository('LiveVotingBundle:Event')->findAll();
+		$allVotes = $this->getDoctrine()->getRepository('LiveVotingBundle:Vote')->findAll();
 
 		return $this->render('LiveVotingBundle:PrizeDraw:index.html.twig',
-            array('events'=>$events)
+            array(
+            	'events' => $events,
+            	'allVotes' => $allVotes
+            	)
         );
 	}
+
+	public function generatePoolAction(Request $request){
+
+		
+		return $this->render('LiveVotingBundle:PrizeDraw:generatePool.html.twig');
+
+	}
+
+
 }
 
 ?>
