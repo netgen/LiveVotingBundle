20317d064d7a7e8a0607ddfce1f3f8a42c440acc
ZvoSus <crodoctor@gmail.com>
2014-08-26 16:47:41 +0200

    We did somethng about login

---
+++ Controller/UserSecurityController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Netgen\LiveVotingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+
+class UserSecurityController extends Controller
+{
+    public function loginAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+
+        return $this->render(
+            'LiveVotingBundle:Security:userlogin.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+}
---
+++ Entity/WebserviceUserProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Netgen\LiveVotingBundle\Entity;
+
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+
+class WebserviceUserProvider implements UserProviderInterface
+{
+    public function loadUserByUsername($username)
+    {
+        // make a call to your webservice here
+        $userData = $this->request->get('webservice_user_provider');
+
+        // pretend it returns an array on success, false if there is no user
+
+        if ($userData) {
+            $password = '...';
+
+            // ...
+
+            return new User($username, $password, $salt, $roles);
+        }
+
+        throw new UsernameNotFoundException(
+            sprintf('Username "%s" does not exist.', $username)
+        );
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof WebserviceUser) {
+            throw new UnsupportedUserException(
+                sprintf('Instances of "%s" are not supported.', get_class($user))
+            );
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    public function supportsClass($class)
+    {
+        return $class === 'Acme\WebserviceUserBundle\Security\User\WebserviceUser';
+    }
+}
+
+?>
