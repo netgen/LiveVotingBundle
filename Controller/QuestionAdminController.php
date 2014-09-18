9a92cd2b3f62de08816af3e26150fa8514d8ff8e
Matija <mburisa@tvz.hr>
2014-09-18 15:59:36 +0200

    QuestionAdminController fix, view

--- Controller/QuestionAdminController.php
+++ Controller/QuestionAdminController.php
@@ -3,10 +3,9 @@
 namespace Netgen\LiveVotingBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-use Netgen\LiveVotingBundle\Entity\Presentation;
-use Netgen\LiveVotingBundle\Form\PresentationType;
+use Netgen\LiveVotingBundle\Entity\Question;
 
 /**
  * Question controller.
@@ -21,9 +20,18 @@ class QuestionAdminController extends Controller
      */
     public function indexAction($event_id)
     {
-        echo("Hi");
+        $em = $this->getDoctrine()->getManager();
+        $event = $em->getRepository('LiveVotingBundle:Event')->find($event_id);
+        $entities = $em->getRepository('LiveVotingBundle:Question')->findBy(array('event'=>$event));
 
-        return $this->render('LiveVotingBundle:Presentation:index.html.twig', null);
+        $that = $this;
+        return $this->render('LiveVotingBundle:Question:index.html.twig', array(
+            'entities' => array_map(
+                function($ent) use ($that) {
+                   return array($ent, $that->createEnableDisableForm($ent)->createView());
+                }, $entities),
+            'event' => $event
+        ));
     }
 
 }
