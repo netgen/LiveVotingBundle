3dc793b645efe644a858682e4dc54d9808726e85
marijo-pavlov <pavlov.marijo@gmail.com>
2015-07-23 12:39:52 +0200

    Added presentation images

---
+++ Entity/PresentationImage.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace Netgen\LiveVotingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PresentationImage
+ */
+class PresentationImage
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var \Netgen\LiveVotingBundle\Entity\Presentation
+     */
+    private $presentation;
+
+    /**
+     * @var \Netgen\LiveVotingBundle\Entity\User
+     */
+    private $user;
+
+    /**
+    * @var \Symfony\Component\HttpFoundation\File\UploadedFile
+    */
+    private $file;
+
+    public function setFile($file){
+      $this->file = $file;
+
+      return $this;
+    }
+
+    public function getFile(){
+      return $this->file;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return PresentationImage
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return PresentationImage
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set presentation
+     *
+     * @param \Netgen\LiveVotingBundle\Entity\Presentation $presentation
+     * @return PresentationImage
+     */
+    public function setPresentation(\Netgen\LiveVotingBundle\Entity\Presentation $presentation = null)
+    {
+        $this->presentation = $presentation;
+
+        return $this;
+    }
+
+    /**
+     * Get presentation
+     *
+     * @return \Netgen\LiveVotingBundle\Entity\Presentation
+     */
+    public function getPresentation()
+    {
+        return $this->presentation;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Netgen\LiveVotingBundle\Entity\User $user
+     * @return PresentationImage
+     */
+    public function setUser(\Netgen\LiveVotingBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Netgen\LiveVotingBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->image
+            ? null
+            : $this->getUploadDir().'/'.$this->getName();
+    }
+
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/bundles/livevoting/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'img/presentations';
+    }
+
+
+    public function upload()
+    {
+        // the file property can be empty if the field is not required
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // use the original file name here but you should
+        // sanitize it at least to avoid any security issues
+
+        // move takes the target directory and then the
+        // target filename to move to
+        $this->getFile()->move(
+            $this->getUploadRootDir(),
+            $this->getFile()->getClientOriginalName()
+        );
+
+        // set the path property to the filename where you've saved the file
+        $this->path = $this->getUploadDir().'/'.$this->getFile()->getClientOriginalName();
+    }}
