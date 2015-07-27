d4c4642e90ed02384240cabc93f46d8c221ace5a
mpetrunic <marin.petrunic@gmail.com>
2015-07-27 16:37:00 +0200

    Implemented save and findOne methods of MemcachedPresentationRepo Written unit test for MemcachedPresentationRepo

--- Tests/Util/MemcachePresentationRepositoryTest.php
+++ Tests/Util/MemcachePresentationRepositoryTest.php
@@ -2,20 +2,48 @@
 
 namespace Netgen\LiveVotingBundle\Tests\Util;
 
+use Netgen\LiveVotingBundle\Service\PresentationService\Impl\MemcachedPresentationRepo;
+use Netgen\LiveVotingBundle\Service\PresentationService\Record\PresentationRecord;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class MemcachePresentationRepositoryTest extends KernelTestCase {
+class MemcachePresentationRepositoryTest extends WebTestCase {
 
+    /**
+     * @var MemcachedPresentationRepo
+     */
     private $memcachePresentationRepo;
 
     public function setUp()
     {
         self::bootKernel();
-        $this->memcachePresentationRepo = static::$kernel->getContainer()->get('live_voting.memcached_presentation_repo');
+        $this->memcachePresentationRepo = new MemcachedPresentationRepo();
     }
 
-    public function testSave() {
-        var_dump($this->memcachePresentationRepo);
+    public function testSavePresentation() {
+        $presentation = $this->createPresentationRecord();
+        $presentation = $this->memcachePresentationRepo->save($presentation);
+        assert($presentation == $this->memcachePresentationRepo->findOne($presentation->getId()),
+            "Saved presentation is not equal retrived presentation!");
+    }
+
+    /**
+     * @return PresentationRecord
+     */
+    private function createPresentationRecord()
+    {
+        $presentation = new PresentationRecord();
+        $presentation->setDescription("asafsdfsdfs");
+        $presentation->setBegin(new \DateTime());
+        $presentation->setJoindInId("asdasdas");
+        $presentation->setEnd(new \DateTime());
+        $presentation->setEventId(123123);
+        $presentation->setHall("D308");
+        $presentation->setImageUrl("asdasdasdasd");
+        $presentation->setName("sadasdas");
+        $presentation->setUserId(123);
+        $presentation->setVotingEnabled(true);
+        return $presentation;
     }
 
 }
