4b0f003f71df94c9aea79fa81bbe2e24d149fe9d
mpetrunic <marin.petrunic@gmail.com>
2015-07-27 13:19:09 +0200

    Presentation abstraction interface and services

---
+++ Service/PresentationService/Record/PresentationRecord.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marin
+ * Date: 27.07.15.
+ * Time: 12:35
+ */
+
+namespace Netgen\LiveVotingBundle\Service\PresentationService\Record;
+
+
+class PresentationRecord {
+
+}
