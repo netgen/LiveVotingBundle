a3af5746418fa4ac2e8e698414bee1299b1b5860
mpetrunic <marin.petrunic@gmail.com>
2015-07-28 15:27:18 +0200

    Custom exception message response overriden from twigbundle

--- LiveVotingBundle.php
+++ LiveVotingBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LiveVotingBundle extends Bundle
 {
+    public function getParent()
+    {
+        return 'TwigBundle';
+    }
 }
---
+++ Resources/views/Exception/exception.html.twig
@@ -0,0 +1,1 @@
+<h1>asdasdasdasd</h1>
