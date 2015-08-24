bb39db6323318e2f701a6f445a683188d3b2e871
joekurta <martin.kurtoic@gmail.com>
2015-08-24 11:26:06 +0200

    Created shell script for sending activation emails

---
+++ Command/ActivationEmailsCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Netgen\LiveVotingBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ActivationEmailsCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('activation:send')
+            ->setDescription('Send activation emails to all users')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $users = $this->getContainer()->get('doctrine')->getRepository('LiveVotingBundle:User')->findAll();
+        $num = 0;
+        foreach ($users as $user) {
+            $user_email = $user->getEmail();
+            $emailHash = md5($this->getContainer()->getParameter('email_hash_prefix') . $user_email);
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Say hello to SummerCamp2015!')
+                ->setFrom('summercamp@netgen.hr')
+                ->setTo($user_email)
+                ->setBody(
+                    $this->getContainer()->get('templating')->render(
+                        'LiveVotingBundle:Email:login.html.twig',
+                        array('emailHash' => $emailHash)
+                    ),
+                    'text/html'
+                );
+            $this->getContainer()->get('mailer')->send($message);
+            $num++;
+        }
+        $output->writeln('Activation mail has been sent to '.$num.' users');
+    }
+}
