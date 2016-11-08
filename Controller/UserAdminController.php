3ac6225e0cc7f13e5889b1b3bd9b1ac3675c009b
tbuljevic <tbuljevic@gmail.com>
2016-11-08 10:16:49 +0100

    removed undefined namespace usage

--- Controller/UserAdminController.php
+++ Controller/UserAdminController.php
@@ -4,7 +4,6 @@ namespace Netgen\LiveVotingBundle\Controller;
 
 use Netgen\LiveVotingBundle\Entity\Event;
 use Netgen\LiveVotingBundle\Entity\UserEventAssociation;
-use Netgen\LiveVotingBundle\Event\UserEvent;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
