5278f94d68c74c07fa2f05d8e78faef82ea38bb5
tbuljevic <tbuljevic@gmail.com>
2016-11-08 10:04:30 +0100

    UserEventAssociation Entity and definition files

---
+++ Entity/UserEventAssociation.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Netgen\LiveVotingBundle\Entity;
+
+/**
+ * UserEventAssociation
+ */
+class UserEventAssociation
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $userId;
+
+    /**
+     * @var integer
+     */
+    private $eventId;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     *
+     * @return UserEventAssociation
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set eventId
+     *
+     * @param integer $eventId
+     *
+     * @return UserEventAssociation
+     */
+    public function setEventId($eventId)
+    {
+        $this->eventId = $eventId;
+
+        return $this;
+    }
+
+    /**
+     * Get eventId
+     *
+     * @return integer
+     */
+    public function getEventId()
+    {
+        return $this->eventId;
+    }
+}
+
